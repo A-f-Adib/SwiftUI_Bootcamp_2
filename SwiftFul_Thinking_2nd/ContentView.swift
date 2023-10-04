814b5edbb70fad40873352f7657cb59a701818cd
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-10-05 02:28:08 +0600

    Initial Commit

---
+++ SwiftFul_Thinking_2nd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftFul_Thinking_2nd
+//
+//  Created by A.f. Adib on 10/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftFul_Thinking_2nd/SwiftFul_Thinking_2ndApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFul_Thinking_2ndApp.swift
+//  SwiftFul_Thinking_2nd
+//
+//  Created by A.f. Adib on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFul_Thinking_2ndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
